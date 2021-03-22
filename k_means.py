c1adcfe96a0e499fa2befa582aeaff4f6e098417
Edgar Baquero <edgar.baquero@cimat.mx>
2021-03-21 22:45:10 -0600

    An implementation of k-means, kernel k-means and GMM algorithms for data clustering

--- k_means.py
+++ k_means.py
@@ -1,48 +1,64 @@
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 from scipy.stats import multivariate_normal
+
 class Cluster:
     #----------------------------Constructor----------------------------
-    def __init__(self, data = None, method = 'k_means', k = None, iter = 5):
+    def __init__(self, method = 'k_means', k = None, iter = 5, kernel_type = 'gaussian'):
         """
         Current class performs data classification under numeric data,
-        using distinct algorithms (k_means, GMM_EM, agglomerative)
+        using distinct algorithms (k_means, GMM_EM, kk_means)
+        
+        Parameters
+        ----------            
+        method: str
+            Method that performs the classification. It'll be 'k_means' by default.
+            You can choose also the 'GMM_EM' method, based on Gaussian Mixture Models
+            or the 'kk_means' method, based on manifold learning clustering
+            
+        k: int
+            Number of clusters.
+            It'll be None by default.
+            
+        iter: int
+            Number of iterations.
+            Number of iterations that we want to compute using k_means method.
+            It'll be 5 by default
+        
+        kernel_type: str
+            kernel type: 'gaussian', 'polynomial'.
+            kernel type used to compute manifold distances when using kernel 
+            k-means ('kk_means') method.
+            It'll be 'gaussian' by default
+        """
+        #------------------Class attributtes------------------
+        self.method = method
+        self.k = k
+        self.iter = iter
+        self.kernel_type = kernel_type
+    #----------------------Class Methods----------------------
+    def fit(self, data = None):
+        """
+        fit the data
         
         Parameters
         ----------
         data : DataFrame, Series, None
             Dataset to classify.
             If data is not set, it will be None by default.
-            
-        method: str
-            Method that performs the classification. It'll be 'k_means' by default.
-            You can choose also the 'agglomerative' method or the 'GMM_EM' method,
-            based on Gaussian Mixture Models
-            
-        k: int
-            Number of clusters.
-            It'll be None by default.
-            
-        iter:
-            Number of iterations.
-            Number of iterations that we want to compute using k_means method.
-            It'll be 5 by default
         """
-        #------------------Class attributtes------------------
-        self.data = data
-        self.method = method
-        self.k = k
-        self.iter = iter
+        if data is None:
+            self.data = None
+            raise TypeError
+        else:
+            self.data = data
+            
+    def transform(self):
+        """
+        transform data using chosen method.
         
-    #----------------------Class Methods----------------------
-    def fit_transform(self):
-        """
-        fit to data, then transform it.
         Return
         ----------
-        original dataframe with corresponding clusters
+        original dataframe with corresponding clusters.
         """
         if self.data is None:
             raise TypeError
@@ -51,29 +67,50 @@ class Cluster:
                 return self.__k_means()
             if self.method == 'GMM_EM':
                 return self.__GMM()
-            if self.method == 'agglomerative':
-                return self.__agglomerative()   
-    #------------------------------------------------------------------------
-    def __agglomerative(self):
-        pass
-    #------------------------------------------------------------------------
+            if self.method == 'kk_means':
+                return self.__kk_means()  
+             
+    def fit_transform(self, data):
+        """
+        fit to data, then transform it.
+        
+        Return
+        ----------
+        original dataframe with corresponding clusters
+        """
+        if data is None:
+            self.data = None
+            raise TypeError
+        else:
+            self.data = data
+            if self.method == 'k_means':
+                return self.__k_means()
+            if self.method == 'GMM_EM':
+                return self.__GMM()
+            if self.method == 'kk_means':
+                return self.__kk_means()
+    #-----------------------------Private methods----------------------------
+    
+    
+    #k-means implementation
     def __k_means(self):
         n,d = self.data.shape
         
-        #first, we set k (the number of clusters) random points to be reloaded in every iteration
-        means = [np.random.randint(max(np.max(self.data)), size=2) for i in range(self.k)]
-        #means = [np.array([0,0]),np.array([9,9])]
+        #Selecting k distinct random means based on data to initialize the parameters
+        random_row = np.random.randint(low=0, high=n, size = self.k)
+        means = [ self.data.iloc[row_index,:] for row_index in random_row ]
+
         
         #creating an array wich contains the corresponding cluster:
          
-        for iter in range(self.iter):
+        for _ in range(self.iter):
             cluster = []
             for row_ix in range(n):
                 #encoding:
                 distances = [self.__l2_distance(self.data.iloc[row_ix], mean) for mean in means]
                 cluster.append(np.argmin(distances))
             #reloading means:
-            print(cluster)        
+              
             for k in range(self.k):
                 #getting indices of the data related to cluster k
                 k_ix = [ix for ix, clus in enumerate(cluster) if clus == k]
@@ -96,7 +133,8 @@ class Cluster:
             coordinates points to compute distance
         """
         return np.sqrt(np.dot(x-y,x-y))
-    #------------------------------------------------------------------------
+    
+    #Gaussian Mixture Model implementation for data clustering
     def __GMM(self):
         #initialization of model parameters
         n, m = self.data.shape
@@ -110,7 +148,7 @@ class Cluster:
         sigma = [ np.cov(self.data.T) for _ in range(self.k) ]
         
         #Iterating model with Expectation-Maximization algorithm
-        for iteration in range(self.iter):
+        for _ in range(self.iter):
             #1.Expectation Step:
             
             
@@ -144,26 +182,67 @@ class Cluster:
         return GMM_df
     
     
-#proving data
+    #kernel k-means implementation
+    def __kk_means(self):
+        n,d = self.data.shape
+        
+        #Selecting k distinct random means based on data to initialize the parameters
+        random_row = np.random.randint(low=0, high=n, size = self.k)
+        means = [ self.data.iloc[row_index,:] for row_index in random_row ]
 
+        
+        #creating an array wich contains the corresponding cluster:
+         
+        for _ in range(self.iter):
+            cluster = []
+            for row_ix in range(n):
+                #encoding based on kernel type:
+                if self.kernel_type == 'gaussian':    
+                    distances = [self.__gaussian_distance(self.data.iloc[row_ix], mean) for mean in means]
+                if self.kernel_type == 'polynomial':    
+                    distances = [self.__polynomial_distance(self.data.iloc[row_ix], mean) for mean in means]
+                    
+                cluster.append(np.argmin(distances))
+            #reloading means:
+            
+            for k in range(self.k):
+                #getting indices of the data related to cluster k
+                k_ix = [ix for ix, clus in enumerate(cluster) if clus == k]
+                #selecting kth cluster's data 
+                cluster_k = self.data.iloc[k_ix]
+                #reloading means:
+                means[k] = np.array(cluster_k.mean(axis = 0))
+                
+        #returning clusterized dataframe:
+        KKMdf = self.data.copy()
+        KKMdf['cluster'] = cluster
+        return KKMdf
+    
+    def __gaussian_kernel(self, x, y):
+        """
+        Returns the kernel related to the two points 
+        given by the transformation of gaussian kernel
+        
+        Parameters
+        ----------
+        x, y: np.array
+            coordinates points to compute the kernel
+        """
+        #It's important to note that we are fixing sigma = 1.5
+        l2_distance = self.__l2_distance(x,y)
+        sigma = 1.5
+        e = 2.718281828
+        return e**(-(l2_distance**2)/(2*(1.5)**2))
 
+    #filling kernel matrix
+    def __kernel_matrix(self, method)
+        n = self.data.shape[0]
+        self.K = np.zeros((n,n))
+        for i in range(n):
+            for j in range(n):
+                if method == 'gaussian':
+                    self.K[i][j] = self.__gaussian_kernel(self.data.iloc[i],self.data.iloc[j])
+        
 
-if __name__ == "__main__":
-    #defining 2 remarkable data groups:
-    data_example = pd.DataFrame({'x':[0,1,2,1,0,6,7,5,6,8,7],
-                             'y':[1,3,2,1,1,9,6,7,5,6,8]})
     
-    #proving example with k-means:
     
-    cluster = Cluster(data = data_example, method = 'k_means', k = 2, iter = 10)
-    kmeans_clustering = cluster.fit_transform()
-    print(kmeans_clustering)
-    sns.scatterplot(data = kmeans_clustering, x = kmeans_clustering.x, y = kmeans_clustering.y,  hue = 'cluster')
-    #plt.show()
-    
-    #proving example with GMM:
-    cluster_gmm = Cluster(data = data_example, method = 'GMM_EM', k = 2, iter = 100)
-    gmm_clustering = cluster_gmm.fit_transform()
-    print(gmm_clustering)
-    sns.scatterplot(data = gmm_clustering, x = gmm_clustering.x, y = gmm_clustering.y,  hue = 'cluster')
-    plt.show()
